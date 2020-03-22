6610451f8de67075e10a27d121fec24f7195956e
Howard Chen <hchen7913@gmail.com>
2020-03-22 12:44:46 -0700

    Move screen directory to own file

--- App.tsx
+++ App.tsx
@@ -15,47 +15,7 @@ import { IconButton, Icon, ScreenHeader } from 'src/Components/Styled/Styled';
 import ActiveTransaction from 'src/Models/common/Transaction';
 import { countOfThings } from 'src/Screens/Prototypes/TestScreen';
 import { ScreenParams } from 'src/common/Navigator';
-
-const ScreenDirectory: Record<keyof Omit<ScreenParams, "None" | "StreakCycle">, any> = {
-  Home: {
-      screen: Screens.Home,
-    }
-  , Goal: Screens.Goal
-  , Goals: Screens.GoalList
-  , Menu: Screens.Menu
-  , AddGoal: Screens.AddGoal
-  , Streak: Screens.Streak
-  , AddTask: Screens.AddTask
-  , Tasks: Screens.TaskList
-  , Reward: Screens.Reward
-  , Rewards: Screens.RewardList
-  , AddReward: Screens.AddReward
-  , Task: Screens.Task
-  , RewardOptions: Screens.RewardOptions
-  , EarnedReward: Screens.EarnedReward
-  , EarnedRewards: Screens.EarnedRewardList
-  , UnusedEarnedRewards: Screens.UnusedEarnedRewards
-  , Penalty: Screens.Penalty
-  , Penalties: Screens.PenaltyList
-  , AddPenalty: Screens.AddPenalty
-  , HomePage : Screens.HomePage
-  , AppStart : Screens.AppStart
-  , RemainingTasks : Screens.RemainingTasks
-  , Overdue: Screens.Overdue
-  , InProgressGoals: Screens.InProgressGoalList
-  , SpecificTask: Screens.SpecificTask
-  , SpecificTaskLists: Screens.SpecificTaskList
-  , Recurrings: Screens.RecurringList
-  , Recur: Screens.Recur
-  , AddRecur: Screens.AddRecur
-  , EarnedPenalties: Screens.EarnedPenaltyList
-  , EarnedPenalty: Screens.EarnedPenalty
-  , UnusedEarnedPenalties: Screens.UnusedEarnedPenalties
-  , Lists: Screens.Lists
-  , Test: Screens.Test
-  , StreakCycles: Screens.StreakCycles
-  , Star: Screens.Star
-};
+import { ScreenDirectory } from "src/common/NavigatorScreens";
 
 
 
---
+++ src/common/NavigatorScreens.ts
@@ -0,0 +1,44 @@
+
+import { ScreenParams } from 'src/common/Navigator';
+import * as Screens from "src/Screens";
+
+export const ScreenDirectory: Record<keyof Omit<ScreenParams, "None" | "StreakCycle">, any> = {
+  Home: {
+      screen: Screens.Home,
+    }
+  , Goal: Screens.Goal
+  , Goals: Screens.GoalList
+  , Menu: Screens.Menu
+  , AddGoal: Screens.AddGoal
+  , Streak: Screens.Streak
+  , AddTask: Screens.AddTask
+  , Tasks: Screens.TaskList
+  , Reward: Screens.Reward
+  , Rewards: Screens.RewardList
+  , AddReward: Screens.AddReward
+  , Task: Screens.Task
+  , RewardOptions: Screens.RewardOptions
+  , EarnedReward: Screens.EarnedReward
+  , EarnedRewards: Screens.EarnedRewardList
+  , UnusedEarnedRewards: Screens.UnusedEarnedRewards
+  , Penalty: Screens.Penalty
+  , Penalties: Screens.PenaltyList
+  , AddPenalty: Screens.AddPenalty
+  , HomePage : Screens.HomePage
+  , AppStart : Screens.AppStart
+  , RemainingTasks : Screens.RemainingTasks
+  , Overdue: Screens.Overdue
+  , InProgressGoals: Screens.InProgressGoalList
+  , SpecificTask: Screens.SpecificTask
+  , SpecificTaskLists: Screens.SpecificTaskList
+  , Recurrings: Screens.RecurringList
+  , Recur: Screens.Recur
+  , AddRecur: Screens.AddRecur
+  , EarnedPenalties: Screens.EarnedPenaltyList
+  , EarnedPenalty: Screens.EarnedPenalty
+  , UnusedEarnedPenalties: Screens.UnusedEarnedPenalties
+  , Lists: Screens.Lists
+  , Test: Screens.Test
+  , StreakCycles: Screens.StreakCycles
+  , Star: Screens.Star
+};
